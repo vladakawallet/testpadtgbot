e6c010eb7b8c710b89b482e55fa9ac9f91b1c524
vlad <156248650+vladakawallet@users.noreply.github.com>
2024-01-26 22:28:12 +0200

    Update

--- database.py
+++ database.py
@@ -11,7 +11,6 @@ from config import DB_HOST, DB_NAME, DB_PASS, DB_PORT, DB_USER
 
 
 DATABASE_URL = f'postgresql+asyncpg://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}?async_fallback=True'
-# DATABASE_URL = f'postgresql://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}'
 
 
 engine = create_async_engine(DATABASE_URL)
